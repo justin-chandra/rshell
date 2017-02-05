e3f4beb7cf7a60c4ba3256f6e3f7a3f0772159f5
Justin Chandra <jchan110@ucr.edu>
2017-02-04 21:24:36 -0800

    Added starting files and a script to create other files

---
+++ src/.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+\n
+#ifndef _H
+#define _H
+\n
+class 
+{
+	public:
+();
+\n
+	private:
+};
+\n
+#endif
---
+++ src/create_files.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo "#include <iostream>" >> $1.h
+echo "" >> $1.h
+echo "using namespace std;" >> $1.h
+
+echo "" >> $1.h
+echo "#ifndef $1_H" >> $1.h
+echo "#define $1_H" >> $1.h
+
+echo "" >> $1.h
+echo "class $1" >> $1.h
+echo "{" >> $1.h
+echo "	public:" >> $1.h
+echo "	$1();" >> $1.h
+echo "	~$1()" >> $1.h
+echo "" >> $1.h
+echo "	private:" >> $1.h
+echo "};" >> $1.h
+
+echo "" >> $1.h
+echo "#endif" >> $1.h
+
+echo  "#include <iostream>" >> $1.cpp
+echo "" >> $1.cpp
+echo "using namespace std;" >> $1.cpp
+
+echo "" >> $1.cpp
+echo "$1::$1()" >> $1.cpp
+echo "{" >> $1.cpp
+echo "" >> $1.cpp
+echo "}" >> $1.cpp
+
+echo "" >> $1.cpp
+echo "$1::~$1()" >> $1.cpp
+echo "{" >> $1.cpp
+echo "" >> $1.cpp
+echo "}" >> $1.cpp
