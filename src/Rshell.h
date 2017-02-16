416cc8337d6b3c2b10197abb49ec0358770bac3a
Audrey Der <ader003@ucr.edu>
2017-02-15 19:37:43 -0800

    Rshell.h set parent, first, second; get parent

--- src/Rshell.h
+++ src/Rshell.h
@@ -10,9 +10,15 @@ class Rshell
 	public:
 	Rshell * first;
 	Rshell * second;
+	Rshell * parent;
 	string userInput;
 	bool status;
-	
+
+	virtual Rshell * getParent() = 0;
+	virtual void setParent(Rshell *) = 0;
+	virtual void setFirst(Rshell *) = 0;
+	virtual void setSecond(Rshell *) = 0;
+
 	virtual bool evaluate() = 0;
 };
 
