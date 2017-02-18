cdfb59c582306a15654ae4e68852abdfac7bd7c2
Justin Chandra <jchan110@ucr.edu>
2017-02-17 16:27:18 -0800

    sdfsdf || should work now

--- src/Command.cpp
+++ src/Command.cpp
@@ -37,14 +37,6 @@ Command::Command(stack<char *> s)
 		s.pop();
 	}
 	v.push_back('\0');
-	/*
-	   cout << "Making: ";
-	   for (unsigned i = 0; v[i] != '\0'; ++i)
-	   {
-	   cout << v[i];
-	   }
-	   cout << endl;
-	   */
 }
 
 Command::Command(char * temp)
@@ -59,15 +51,13 @@ Command::~Command()
 
 bool Command::evaluate()
 {
-	//pid_t parent = getpid();
 	string exit = "exit";
 	int status;
 	if (!v.empty())
 	{
 		if (v.at(0) == exit)
 		{
-			//_exit(1);
-			std::exit(1);
+			_exit(1);
 		}
 	}
 	pid_t child_pid = fork();
@@ -82,25 +72,19 @@ bool Command::evaluate()
 		if (execvp(v.at(0), vloc) == -1)
 		{
 			cout << "-rshell: " << v.at(0) << ": command not found" << endl;
-			//the or issue with first thing is taken care of if the line below
-			//is taken out
-			_exit(-1); 
-			//cout << "returning false in execvp if" << endl;
-			//return false;
+			_exit(1); 
 		}
-		//_exit(1);
-		//cout << "returning true" << endl;
-		//return true;
 	}
 	else if (child_pid > 0)
 	{
-		waitpid(child_pid, &status, 0);
-		/*
-		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+		if (waitpid(child_pid, &status, 0) == -1)
 		{
-			return status;
+			//cout << "wait error" << endl;
 		}
-		*/
+		if (WEXITSTATUS(status) != 0)
+		{
+			return false;
+		}
 	}
 	return true;
 }
