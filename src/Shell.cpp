bb9e1591c5e924f72457ff0cc5dd9f15e7b5b8be
Justin Chandra <jchan110@ucr.edu>
2017-02-19 00:25:00 -0800

    Overhaul Adding a new class to interface the user - this will help clean up over 200 lines of code from the main - Will help with encapsulation - gives us another chance to uproot bugs and do things better the second time

---
+++ src/Shell.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#include "Shell.h"
+
+Shell::Shell()
+{
+
+}
+
+Shell::~Shell()
+{
+
+}
---
+++ src/Shell.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef SHELL
+#define SHELL
+
+class Shell
+{
+	public:
+	Shell();
+	~Shell();
+};
+
+#endif
