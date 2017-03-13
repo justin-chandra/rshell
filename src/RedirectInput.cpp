92d675f85b1905d7acdb4afa8a286866cc8e67b6
Justin Chandra <jchan110@ucr.edu>
2017-03-12 18:19:34 -0700

    filled out some redirect files

--- src/RedirectInput.cpp
+++ src/RedirectInput.cpp
@@ -1,1 +1,26 @@
 #include "RedirectInput.h"
+
+RedirectInput::RedirectInput()
+{
+
+}
+
+RedirectInput::RedirectInput(Rshell * first, Rshell * second)
+{
+	
+}
+
+RedirectInput::RedirectInput(Rshell * second)
+{
+	this->second = second;
+}
+
+bool RedirectInput::evaluate()
+{
+	return false;
+}
+
+Rshell * RedirectInput::getParent()
+{
+	return this->parent;
+}
--- src/RedirectInput.h
+++ src/RedirectInput.h
@@ -4,3 +4,24 @@
 using namespace std;
 
 #include "Connector.h"
+
+#ifndef REDIRIN
+#define REDIRIN
+
+class RedirectInput: public Connector
+{
+	public:
+		Rshell * first;
+		Rshell * second;
+		Rshell * parent;
+		bool status;
+
+		RedirectInput();
+		RedirectInput(Rshell *, Rshell *);
+		RedirectInput(Rshell *);
+		
+		bool evaluate();
+		Rshell * getParent();
+};
+
+#endif
